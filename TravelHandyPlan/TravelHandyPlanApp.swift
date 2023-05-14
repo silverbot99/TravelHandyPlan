3d38f8332ba175be5ff77ec05039f34dac1c713a
silverbot <nhanhuynh.kim23@gmail.com>
2023-05-14 16:41:56 +0700

    Initial Commit

---
+++ TravelHandyPlan/TravelHandyPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelHandyPlanApp.swift
+//  TravelHandyPlan
+//
+//  Created by Ngan Huynh on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelHandyPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
